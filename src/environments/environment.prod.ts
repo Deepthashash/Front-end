295882885bb84cf8af9d57338f06b96480d108cd
Deeptha Shashimina <deepthashashimina96@gmail.com>
2021-03-13 15:36:31 +0530

    Fixed bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBjm9MhgX7fgRyWaxb8OKGcY6ArURQrRxk",
+    authDomain: "project-2b6bc.firebaseapp.com",
+    projectId: "project-2b6bc",
+    storageBucket: "project-2b6bc.appspot.com",
+    messagingSenderId: "1011048677001",
+    appId: "1:1011048677001:web:879c07415440ffacbe1058",
+    measurementId: "G-38QVQW3BD5"
+  }
 };
