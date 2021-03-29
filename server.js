c8551e42b47f3ba95eee7bf70ba494868b85f3d2
Deeptha Shashimina <deepthashashimina96@gmail.com>
2021-03-29 12:35:03 +0530

    Fixed bugs

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname));
-
-// app.get('/*', (req, res) =>
-//     res.sendFile(path.join(__dirname + '/index.html')),
-// );
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
+//Install express server
 const express = require('express');
+const path = require('path');
 
-var app = express();
-app.use(express.static('./dist/Front-end'));
+const app = express();
 
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/Front-end/'});
-});
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Front-end'));
 
+app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname + '/dist/Front-end/index.html')),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// const express = require('express');
+
+// var app = express();
+// app.use(express.static('./dist/Front-end'));
+
+// app.get('/*', function(req, res) {
+//   res.sendFile('index.html', {root: 'dist/Front-end/'});
+// });
+
+// app.listen(process.env.PORT || 8080);
